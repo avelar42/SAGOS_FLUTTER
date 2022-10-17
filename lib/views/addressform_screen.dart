95e8f80860c38c9a107e8eac3c01b1ca54a0f2ff
Daniel <avelar42@hotmail.com>
2022-10-17 20:21:54 -0300

    Implementation of body address form screen

--- lib/views/addressform_screen.dart
+++ lib/views/addressform_screen.dart
@@ -8,6 +8,8 @@ class AddressFormScreen extends StatefulWidget {
 }
 
 class _AddressFormScreenState extends State<AddressFormScreen> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +17,39 @@ class _AddressFormScreenState extends State<AddressFormScreen> {
         title: Text('Cadastro de endereço'),
         actions: [IconButton(onPressed: () {}, icon: Icon(Icons.save))],
       ),
+      body: Padding(
+          padding: EdgeInsets.all(10),
+          child: Form(
+              key: _formKey,
+              child: ListView(
+                children: [
+                  //Text('Codigo: 1'),
+                  TextFormField(
+                      decoration: InputDecoration(label: Text('CEP')),
+                      keyboardType: TextInputType.number,
+                      textInputAction: TextInputAction.next),
+                  TextFormField(
+                    decoration: InputDecoration(label: Text('Rua')),
+                    keyboardType: TextInputType.name,
+                    textInputAction: TextInputAction.next,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(label: Text('Numero')),
+                    keyboardType: TextInputType.number,
+                    textInputAction: TextInputAction.next,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(label: Text('Bairro')),
+                    keyboardType: TextInputType.name,
+                    textInputAction: TextInputAction.next,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(label: Text('Cidade')),
+                    keyboardType: TextInputType.name,
+                    textInputAction: TextInputAction.done,
+                  ),
+                ],
+              ))),
     );
   }
 }
